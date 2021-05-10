10b71bfcff3205b140a17718a33dc1c5c827c608
Vinicius <cpudvini2@gmail.com>
2021-05-10 17:50:19 -0300

    Criado base das telas de login e listagem de carros

---
+++ app/src/main/java/com/brq/applojadecarros/Lista_carros_activity.kt
@@ -0,0 +1,11 @@
+package com.brq.applojadecarros
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Lista_carros_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_carros)
+    }
+}
