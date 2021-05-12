a4efb9767d6dfceb0a381ba96a1be06c957a1fe1
Vinicius <cpudvini2@gmail.com>
2021-05-12 17:34:09 -0300

    Merge entre tela de login e lista de carro

--- app/src/main/java/com/brq/applojadecarros/login_Activity.kt
+++ app/src/main/java/com/brq/applojadecarros/login_Activity.kt
@@ -1,11 +1,29 @@
 package com.brq.applojadecarros
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class login_Activity : AppCompatActivity() {
+
+    lateinit var btnLogin: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        loadComponents()
+
+        btnLogin.setOnClickListener {
+            Intent(this, Lista_carros_activity::class.java).let {
+                startActivity(it)
+            }
+        }
+
+    }
+
+    private fun loadComponents() {
+        btnLogin = findViewById(R.id.login_button)
     }
 }
