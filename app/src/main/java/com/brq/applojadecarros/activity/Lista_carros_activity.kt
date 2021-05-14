f958c850509a3715761f6f3aefd8a6b603657b3b
Vinicius <cpudvini2@gmail.com>
2021-05-14 16:07:28 -0300

    Refatorados as fontes de dados

--- app/src/main/java/com/brq/applojadecarros/activity/Lista_carros_activity.kt
+++ app/src/main/java/com/brq/applojadecarros/activity/Lista_carros_activity.kt
@@ -30,29 +30,7 @@ class Lista_carros_activity : AppCompatActivity(), ItemClickListener {
     }
 
     private fun loadData() {
-
-        Car("Fiat Uno", R.drawable.uno, "Carro de passeio", "Texto grande").apply {
-            listaDeCarros.add(this)
-        }
-        Car("Volkswagen Gol", R.drawable.gol, "Carro de corrida", "Texto grande mesmo").apply {
-            listaDeCarros.add(this)
-        }
-        Car("Fiat Argo", R.drawable.argo, "Carro de passeio", "Texto grande").apply {
-            listaDeCarros.add(this)
-        }
-        Car("Toyota Corolla", R.drawable.corolla, "Carro de passeio", "Texto grande").apply {
-            listaDeCarros.add(this)
-        }
-        Car("Hyundai HB20", R.drawable.hb20, "Carro de passeio", "Texto grande").apply {
-            listaDeCarros.add(this)
-        }
-        Car("Chevrolet Prisma", R.drawable.prisma, "Carro de passeio", "Texto grande").apply {
-            listaDeCarros.add(this)
-        }
-        Car("Jeep Renegade", R.drawable.renegade, "Carro de passeio", "Texto grande").apply {
-            listaDeCarros.add(this)
-        }
-
+        listaDeCarros = Car.getCarList()
     }
 
     private fun loadComponents() {
--- app/src/main/java/com/brq/applojadecarros/model/Pessoa.kt
+++ app/src/main/java/com/brq/applojadecarros/model/Pessoa.kt
@@ -5,5 +5,14 @@ class Pessoa (
     var idade: Int,
     var email: String,
     var senha: String
-        ){
+        )
+{
+
+    companion object{
+        fun getPessoasList(): ArrayList<Pessoa>{
+            val list = ArrayList<Pessoa>()
+            list.add(Pessoa("James", 25, "james@gmail.com","senha1"))
+            return list
+        }
+    }
 }
