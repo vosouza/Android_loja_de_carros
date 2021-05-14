d273a1daf9861b75cfce4612264909d23440cc51
Vinicius <cpudvini2@gmail.com>
2021-05-14 15:32:59 -0300

    reoganizados os arquivos

---
+++ app/src/main/java/com/brq/applojadecarros/activity/Detalhes_Activity.kt
@@ -0,0 +1,12 @@
+package com.brq.applojadecarros.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.brq.applojadecarros.R
+
+class Detalhes_Activity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalhes)
+    }
+}
