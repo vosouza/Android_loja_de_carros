7d6721ad06703a26b47874795c41f4aead1db750
Vinicius <cpudvini2@gmail.com>
2021-05-17 17:32:59 -0300

    feita logica da tela Detalhes_Activity.kt

--- app/src/main/java/com/brq/applojadecarros/activity/Detalhes_Activity.kt
+++ app/src/main/java/com/brq/applojadecarros/activity/Detalhes_Activity.kt
@@ -1,13 +1,68 @@
 package com.brq.applojadecarros.activity
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.brq.applojadecarros.R
+import com.brq.applojadecarros.model.Car
+import com.brq.applojadecarros.model.ItemClickListener
 
-class Detalhes_Activity : AppCompatActivity(){
+class Detalhes_Activity : AppCompatActivity(), ItemClickListener{
+
+
+    lateinit var txtNome: TextView
+    lateinit var imgCarro: ImageView
+    lateinit var txtDescricao1: TextView
+    lateinit var txtDescricao2: TextView
+    lateinit var txtPreco: TextView
+    lateinit var btnOrcamento: Button
+
+    private lateinit var carro : Car
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalhes)
+
+        loadComponents()
+
+        val listaCarros = Car.getCarList()
+        val intent = intent
+        val carId = intent.getIntExtra("CarId", -1)
+
+        if(carId == -1){
+            Toast.makeText(this,"Erro ao localizar o carro", Toast.LENGTH_LONG).show()
+        }else{
+            carro = listaCarros[carId]
+            loadContent()
+        }
+
+    }
+
+    private fun loadContent() {
+        txtNome.text = carro.nomeCarro
+        imgCarro.setImageResource(carro.Image)
+        txtDescricao1.text = carro.tipoCarro
+        txtDescricao2.text = carro.descricao
+        txtPreco.text = "Ainda nao implementado"
+    }
+
+    fun loadComponents(){
+        txtNome = findViewById<TextView>(R.id.txt_nome_veiculo)
+        imgCarro = findViewById<ImageView>(R.id.imagem_car)
+        txtDescricao1 = findViewById<TextView>(R.id.txt_descricao1)
+        txtDescricao2 = findViewById<TextView>(R.id.txt_descricao2)
+        txtPreco = findViewById<TextView>(R.id.txt_Preco)
+        btnOrcamento = findViewById<Button>(R.id.btn_Pedir_Orcamento)
+    }
+
+    override fun onClickItem(view: View?, index: Int) {
+    }
+
+    override fun onLongCLickItem(view: View?, index: Int) {
+        //nao usado
     }
 }
